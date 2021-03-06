ab7a87aa460c7d81c020f34f196235deefebe3cf
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2021-03-06 17:13:42 +0100

    add: add solution

---
+++ PomocnikEpuap.Core/EmergencyDownloadedEpuapFilesPackage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.IO.Compression;
+using System.Xml;
+
+
+
+namespace PomocnikEpuap.Core
+{
+    public class EmergencyDownloadedEpuapFilesPackage
+    {
+        private readonly string packageFilePath;
+
+        public EmergencyDownloadedEpuapFilesPackage(string packageFilePath)
+        {
+            this.packageFilePath = packageFilePath;
+            XmlFileNames = new List<string>();
+            //XmlFiles = new List<XmlDocument>();
+        }
+        public void Open() {
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ZipArchive zipArchive = ZipFile.Open(packageFilePath, ZipArchiveMode.Read, System.Text.Encoding.GetEncoding(852));
+                        
+            foreach (ZipArchiveEntry z in zipArchive.Entries)
+            {
+                XmlFileNames.Add(z.Name);
+                System.IO.Stream s = z.Open();
+                XmlDocument xml = new XmlDocument();
+                xml.Load(s);
+                ////XmlFiles.Add(xml);
+            }
+
+            FilesCount = zipArchive.Entries.Count;
+        }
+
+        public int FilesCount { get; private set; }
+
+        public List<string> XmlFileNames { get; private set; }
+
+        //public List<XmlDocument> XmlFiles { get; private set; }
+
+    }
+}
---
+++ PomocnikEpuap.Core/EpuapDocument.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PomocnikEpuap.Core
+{
+    public class EpuapDocument
+    {
+        //Adresaci (nazwa, adres)
+        //DoWiadomosci (nazwa, adres) Carbon Copy
+
+        public EpuapDocument()
+        { 
+            
+        }
+
+
+        public string Identyfier { get; }
+        public string Title { get; }
+        public DateTime SendDate { get; }
+        public string SenderName { get; }
+        public string SenderBox { get; }
+        public string RecipientName { get; }
+        public string RecipientBox { get; }
+        public string Content { get; }
+        public List<Attachment> Attachements {get;}
+
+    }
+}
