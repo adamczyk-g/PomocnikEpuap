23377070acbf81680f43c7e93e727a62e85271e6
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2021-03-08 15:19:00 +0100

    add: add class Address

---
+++ PomocnikEpuap.Core/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PomocnikEpuap.Core
+{
+    public class Address
+    {
+        public Address(string postalCode, string city, string street, string buildingNumber, string flatNumber)
+        {
+            PostalCode = postalCode;
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+            FlatNumber = flatNumber;
+        }
+
+        public string PostalCode { get; private set; }
+        public string City { get; private set; }
+        public string Street { get; private set; }
+        public string BuildingNumber  { get; private set; }
+        public string FlatNumber { get; private set; }
+    }
+}
